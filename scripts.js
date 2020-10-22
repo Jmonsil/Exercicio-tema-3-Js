f3e49468daddcb44fe60139a73744cfbd055678a
‘[jorge]’ <jm18103797.jm@gmail.com>
2020-10-22 20:27:34 -0300

    exercicio tema 3 JavaScript

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let name=prompt("Digite seu nome");
+let lastname=prompt("Digite seu sobrenome")
+let age=prompt("Digite sua idade")
+let nationality=prompt("Digite sua nacionalidade")
+
+console.log("Olá," +' '+ name);
+console.log("Nome e Sobrenome:" +' '+ name +'  '+ lastname)
+console.log("Idade: " +' '+ age)
+console.log("Nacionalidade: " +' '+ nationality)
+
